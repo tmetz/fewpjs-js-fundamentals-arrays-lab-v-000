2602aa7f8ff738550d148f8f1b2a9579233fec16
Tammy Metz <tammy@tammymetz.com>
2019-10-01 15:37:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
 append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
